8a31d81e1f0c676aa512aed846008579e626b290
Juan Felipe Bahamon Castillo <juan_bahamonca@fet.edu.co>
2026-02-11 12:11:49 -0500

    Agrego URL Front Publica

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200'],
+    'allowed_origins' => ['http://localhost:4200', 'https://dbalance-app.vercel.app/login'],
 
     'allowed_origins_patterns' => [],
 
